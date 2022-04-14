eee80951ee6f8cdf34011128ba8a3fce3df99706
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-04-14 18:50:27 +0300

    cli_muc

--- app/components/custom/shared/analytics_cookie_banner_component.rb
+++ app/components/custom/shared/analytics_cookie_banner_component.rb
@@ -1,4 +1,10 @@
 require_dependency Rails.root.join("app", "components", "shared", "comments_component").to_s
 
 class Shared::AnalyticsCookieBannerComponent < ApplicationComponent
+  def statistic_enabled?
+    (
+      view_context.cookies['statistic_cookies_enabled'] == 'true' ||
+      view_context.current_user&.custom_statistic_cookies_enabled
+    )
+  end
 end
