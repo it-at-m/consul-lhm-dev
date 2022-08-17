c4d96a795e12fe21334672f4eac31cc9968ae46c
Mike Svystun <michael.svystun@gmail.com>
2022-08-17 11:23:37 +0300

    con-1206

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_131411) do
+ActiveRecord::Schema.define(version: 2022_08_17_081843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -2004,6 +2004,7 @@ ActiveRecord::Schema.define(version: 2022_08_11_131411) do
     t.string "street_name"
     t.integer "plz"
     t.string "city_name"
+    t.string "unique_stamp"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["date_of_birth"], name: "index_users_on_date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
