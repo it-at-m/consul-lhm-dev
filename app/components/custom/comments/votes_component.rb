1bcde7daaa74fb026c45d2869e35aaa5d62c2f9d
Mike Svystun <michael.svystun@gmail.com>
2022-08-24 18:51:31 +0300

    con-1305

--- app/components/custom/comments/votes_component.rb
+++ app/components/custom/comments/votes_component.rb
@@ -9,6 +9,9 @@ class Comments::VotesComponent < ApplicationComponent
       if comment.commentable.is_a?(Projekt)
         projekt_feature?(comment.commentable, "general.allow_downvoting_comments")
 
+      elsif comment.commentable.is_a?(Legislation::Annotation)
+        true
+
       else
         projekt_feature?(comment.commentable.projekt, "general.allow_downvoting_comments")
 
