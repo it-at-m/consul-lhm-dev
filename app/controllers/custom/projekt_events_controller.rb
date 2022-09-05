e3b89c647c5c7878546c098abb77061a2fa4bb93
Mike Svystun <michael.svystun@gmail.com>
2022-09-05 12:39:50 +0300

    minor fix

--- app/controllers/custom/projekt_events_controller.rb
+++ app/controllers/custom/projekt_events_controller.rb
@@ -7,12 +7,12 @@ class ProjektEventsController < ApplicationController
   has_filters %w[all incoming past], only: [:index]
 
   before_action do
-    raise FeatureFlags::FeatureDisabled, :projekt_events_page unless Setting['extended_feature.general.enable_projekt_events_page']
+    raise FeatureFlags::FeatureDisabled, :projekt_events_page unless Setting["extended_feature.general.enable_projekt_events_page"]
   end
 
   def index
     @valid_filters = %w[all incoming past]
-    @current_filter = @valid_filters.include?(params[:filter]) ? params[:filter] : @valid_filters.first
+    @current_filter = @valid_filters.include?(params[:filter]) ? params[:filter] : "incoming"
 
     @projekt_events = ProjektEvent.all.page(params[:page]).per(10).send("sort_by_#{@current_filter}")
   end
