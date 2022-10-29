949e4c7e5c1ed2f73650c35eb8b2f669199055e9
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-10-29 15:19:58 +0300

    Update

--- app/components/custom/debates/votes_component.rb
+++ app/components/custom/debates/votes_component.rb
@@ -12,16 +12,17 @@ class Debates::VotesComponent < ApplicationComponent
       if !user_signed_in?
         sanitize(t("custom.users.login_to_vote", signin: link_to_signin, signup: link_to_signup))
 
+      elsif debate.debate_phase.only_citizens_allowed? && current_user&.not_current_city_citizen?
+        t("custom.shared.warnings.only_for_citizens", city: Setting["org_name"])
+
+      elsif debate.debate_phase.only_geozones_allowed? && debate.debate_phase.geozone_not_allowed?(current_user)
+        t("custom.shared.warnings.only_for_geozones", geozones: debate.debate_phase.geozone_restrictions_formated)
+
       elsif current_user.organization?
         t("votes.organizations")
 
       elsif !current_user.level_two_or_three_verified?
         sanitize(t("custom.votes.not_verified", verify_account: link_to_verify_account_short))
-
-      elsif debate.debate_phase &&
-        (debate.debate_phase.geozone_restrictions.any? &&
-          !debate.debate_phase.geozone_restrictions.include?(current_user.geozone))
-        t("custom.votes.geo_restricted")
 
       else
         t("custom.votes.not_votable")
