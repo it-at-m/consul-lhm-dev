a4ca94ce2d9020d9bd61b9172c1799742433f4d2
Michael Svystun <michael.svystun@gmail.com>
2022-11-25 19:21:35 +0200

    con-1397

--- app/components/custom/comments/votes_component.rb
+++ app/components/custom/comments/votes_component.rb
@@ -1,7 +1,7 @@
 require_dependency Rails.root.join("app", "components", "comments", "votes_component").to_s
 
 class Comments::VotesComponent < ApplicationComponent
-  delegate :cannot?, :projekt_feature?, to: :helpers
+  delegate :cannot?, :projekt_feature?, :current_user, to: :helpers
 
   private
 
@@ -9,15 +9,20 @@ class Comments::VotesComponent < ApplicationComponent
       if comment.commentable.is_a?(Projekt)
         projekt_feature?(comment.commentable, "general.allow_downvoting_comments")
 
-      elsif comment.commentable.is_a?(DeficiencyReport)
-        true
-
-      elsif comment.commentable.is_a?(Legislation::Annotation)
+      elsif comment.commentable.is_a?(DeficiencyReport) || comment.commentable.is_a?(Legislation::Annotation)
         true
 
       else
         projekt_feature?(comment.commentable.projekt, "general.allow_downvoting_comments")
 
       end
+    end
+
+    def thumb_up_class
+      return "voted_up" if current_user.voted_up_on? comment
+    end
+
+    def thumb_down_class
+      return "voted_down" if current_user.voted_down_on? comment
     end
 end
