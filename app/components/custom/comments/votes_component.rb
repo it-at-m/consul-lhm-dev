22e8193c87af4a0455350d2d944a57b8817f305a
Michael Svystun <michael.svystun@gmail.com>
2022-12-07 12:07:58 +0200

    minor bug fix

--- app/components/custom/comments/votes_component.rb
+++ app/components/custom/comments/votes_component.rb
@@ -6,11 +6,12 @@ class Comments::VotesComponent < ApplicationComponent
   private
 
     def allow_downvoting?
+      return true if comment.commentable.is_a?(DeficiencyReport) ||
+        comment.commentable.is_a?(Legislation::Annotation) ||
+        comment.commentable.is_a?(Topic)
+
       if comment.commentable.is_a?(Projekt)
         projekt_feature?(comment.commentable, "general.allow_downvoting_comments")
-
-      elsif comment.commentable.is_a?(DeficiencyReport) || comment.commentable.is_a?(Legislation::Annotation)
-        true
 
       else
         projekt_feature?(comment.commentable.projekt, "general.allow_downvoting_comments")
