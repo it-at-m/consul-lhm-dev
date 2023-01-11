55af59669bf60d51b1b4224263cec1f39990b597
Michael Svystun <michael.svystun@gmail.com>
2023-01-11 19:18:40 +0200

    con-1405

--- app/controllers/custom/polls/question_answers_controller.rb
+++ app/controllers/custom/polls/question_answers_controller.rb
@@ -1,8 +1,9 @@
 class Polls::QuestionAnswersController < ApplicationController
+  skip_authorization_check #debugger
 
   def results
     @poll = Poll.find(params[:poll_id])
     @question_answer = Poll::Question::Answer.find(params[:id])
-    debugger
+    @question = @question_answer.question
   end
 end
