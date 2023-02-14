7c074547ae4665a9821272f93d7d0f4d34a10559
Michael Svystun <michael.svystun@gmail.com>
2023-02-14 16:35:38 +0200

    con-1450

--- app/components/custom/proposals/form_component.rb
+++ app/components/custom/proposals/form_component.rb
@@ -1,6 +1,7 @@
 require_dependency Rails.root.join("app", "components", "proposals", "form_component").to_s
 
 class Proposals::FormComponent < ApplicationComponent
+  delegate :projekt_feature?, to: :helpers
   def initialize(proposal, url:, selected_projekt:)
     @proposal = proposal
     @url = url
