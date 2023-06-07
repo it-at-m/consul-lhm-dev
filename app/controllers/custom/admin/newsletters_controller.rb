026ac4900e8f7c3bcc8b0767dc6cdd28ed4e28d8
Michael Svystun <michael.svystun@gmail.com>
2023-06-07 18:54:01 +0300

    con-1541

--- app/controllers/custom/admin/newsletters_controller.rb
+++ app/controllers/custom/admin/newsletters_controller.rb
@@ -6,6 +6,8 @@ class Admin::NewslettersController < Admin::BaseController
     @projekt = Projekt.find(params[:projekt_id]) if params[:projekt_id]
 
     if @projekt.present?
+      @newsletter.subject = t("custom.newsletters.new_projekt.subject", title: @projekt.title)
+      @newsletter.from = Setting["mailer_from_address"]
       @newsletter.segment_recipient = "newsletter_subscribers"
       @newsletter.body = newsletter_body
     end
@@ -15,24 +17,32 @@ class Admin::NewslettersController < Admin::BaseController
 
     def newsletter_body
       body = ""
-      body += "<h1>#{@projekt.title}</h1>" if @projekt.title
+      body += "<h3>#{@projekt.title}</h3>" if @projekt.title
       body += "<p>#{@projekt.description}</p>" if @projekt.description
       body += "<p><img src='#{url_for(@projekt.image.variant(:large))}'></p>" if @projekt.image
 
-      body += "<p>#{t("custom.newsletters.new_projekt.total_duration_start")}: #{l(@projekt.total_duration_start, format: "%d. %B %Y")}</p>"
-      body += "<p>#{t("custom.newsletters.new_projekt.total_duration_end")}: #{l(@projekt.total_duration_end, format: "%d. %B %Y")}</p>"
+      body += "<p><strong>#{t("custom.newsletters.new_projekt.total_duration_start")}:</strong> #{l(@projekt.total_duration_start, format: "%d. %B %Y")}</p>"
+      body += "<p><strong>#{t("custom.newsletters.new_projekt.total_duration_end")}:</strong> #{t("custom.newsletters.new_projekt.total_duration_end_till")} #{l(@projekt.total_duration_end, format: "%d. %B %Y")}</p>"
 
-      body += "<p>#{t("custom.newsletters.new_projekt.open_phases")}:</p>"
-      body += "<ul>#{open_phases_for_body}</ul>"
+      body += "<p><strong>#{t("custom.newsletters.new_projekt.open_phases")}:</strong></p>"
+      body += "<ul style='margin-bottom:30px;'>#{open_phases_for_body}</ul>"
 
-      body += "<p><a href=#{@projekt.page.url}>#{t("custom.newsletters.new_projekt.url")}</a></p>"
+      body += "<p style='margin-bottom:30px;'>"
+      body += "<a href='#{@projekt.page.url}' style='background:#004a83;padding:0.75rem 1.5rem;color:#fff;border-radius:4px;margin-right:20px;'>#{t("custom.newsletters.new_projekt.url")}</a>"
+      body += "<a href='#{@projekt.page.url}#filter-subnav' style='background:#004a83;padding:0.75rem 1.5rem;color:#fff;border-radius:4px;'>#{t("custom.newsletters.new_projekt.url_participate")}</a>"
+      body += "</p>"
 
       body
     end
 
     def open_phases_for_body
-      @projekt.projekt_phases.sorted.map do |phase|
-        "<li>#{phase.title}</li>"
+      @projekt.projekt_phases.where(projekt_phases: { active: true }).sorted.map do |phase|
+        date_range = helpers.format_date_range(phase.start_date, phase.end_date)
+        if date_range.present?
+          "<li>#{phase.title} (#{helpers.format_date_range(phase.start_date, phase.end_date)})</li>"
+        else
+          "<li>#{phase.title}</li>"
+        end
       end.join
     end
 end
