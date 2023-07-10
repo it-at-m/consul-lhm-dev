d2f83a4aff1af02fc34bb95fe66ff66774236ca4
Michael Svystun <michael.svystun@gmail.com>
2023-07-10 14:54:35 +0300

    con-1566

--- app/controllers/custom/projekt_subscriptions_controller.rb
+++ app/controllers/custom/projekt_subscriptions_controller.rb
@@ -6,6 +6,7 @@ class ProjektSubscriptionsController < ApplicationController
     redirect_to new_user_session_path and return unless current_user
 
     @projekt = @projekt_subscription.projekt
+    @custom_page = @projekt.page
 
     @projekt_subscription.update!(active: projekt_subscription_params[:active])
 
