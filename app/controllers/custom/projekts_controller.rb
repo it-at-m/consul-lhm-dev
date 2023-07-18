3eade2c290a2ccf212d0dd59f10da6e10f27b1c3
Michael Svystun <michael.svystun@gmail.com>
2023-07-18 18:54:57 +0300

    minor fix

--- app/controllers/custom/projekts_controller.rb
+++ app/controllers/custom/projekts_controller.rb
@@ -30,7 +30,7 @@ class ProjektsController < ApplicationController
     take_by_sdgs
 
     valid_orders.push("index_order_drafts") if current_user&.administrator? || current_user&.projekt_manager?
-    @active_projekts_orders = valid_orders.select { |order| @projekts.send(order).count > 0 }
+    @active_projekts_orders = valid_orders.select { |order| @projekts.send(order).count > 0 }.presence || ["index_order_all"]
     @current_projekts_order = valid_orders.include?(params[:order]) ? params[:order] : @active_projekts_orders.first
     @special_projekt = Projekt.unscoped.find_by(special: true, special_name: "projekt_overview_page")
     @projekts = @projekts.send(@current_projekts_order)
