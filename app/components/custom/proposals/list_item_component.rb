20e5ba050abf78373ac29cc17732edd60f6a41b2
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-07-28 20:04:07 +0300

    Add proposals new deisgn component

---
+++ app/components/custom/proposals/list_item_component.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Proposals::ListItem < ApplicationComponent
+  attr_reader :proposal
+
+  def initialize(proposal:, wide: false)
+    @proposal = proposal
+    @wide = wide
+  end
+
+  def component_attributes
+    {
+      resource: @proposal,
+      projekt: proposal.projekt,
+      title: proposal.title,
+      description: proposal.summary,
+      tags: proposal.tags.first(3),
+      sdgs: proposal.related_sdgs.first(5),
+      # start_date: proposal.total_duration_start,
+      # end_date: proposal.total_duration_end,
+      wide: @wide,
+      url: helpers.proposal_path(proposal),
+      image_url: proposal.image&.variant(:medium),
+      date: proposal.created_at,
+      image_placeholder_icon_class: "fa-lightbulb",
+      author: proposal.author,
+      id: proposal.id
+    }
+  end
+end
