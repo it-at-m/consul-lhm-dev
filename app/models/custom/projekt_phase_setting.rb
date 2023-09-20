81bb82b21469615ff73da982008db0074e696ea1
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-09-20 12:39:33 +0300

    Update

--- app/models/custom/projekt_phase_setting.rb
+++ app/models/custom/projekt_phase_setting.rb
@@ -57,7 +57,7 @@ class ProjektPhaseSetting < ApplicationRecord
           "feature.general.only_admins_create_proposals": "",
 
           "feature.form.allow_attached_image": "active",
-          # "feature.form.enable_summary": "",
+           "feature.form.enable_summary": "",
           "feature.form.show_map": "active",
           "feature.form.enable_geoman_controls_in_maps": "active",
           "feature.form.allow_attached_documents": "",
