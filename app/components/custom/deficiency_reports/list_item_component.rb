ce4f8f2cdc37507bfd6d65f3454e2c66243da0c9
Michael Svystun <michael.svystun@gmail.com>
2023-09-21 16:26:34 +0300

    con-1361

--- app/components/custom/deficiency_reports/list_item_component.rb
+++ app/components/custom/deficiency_reports/list_item_component.rb
@@ -16,7 +16,17 @@ class DeficiencyReports::ListItemComponent < ApplicationComponent
       url: helpers.deficiency_report_path(deficiency_report),
       image_url: deficiency_report.image&.variant(:card_thumb),
       image_placeholder_icon_class: "fa-lightbulb",
-      author: deficiency_report.author
+      author: deficiency_report.author,
+      subline: subline
     }
+  end
+
+  def subline
+    link_to deficiency_reports_path(dr_categories: deficiency_report.category.id) do
+      tag.i(
+        class: "fas fa-#{deficiency_report.category.icon.presence || "circle"}",
+        style: "color:#{deficiency_report.category.color};margin-right:0.5rem;"
+      ) + deficiency_report.category.name
+    end
   end
 end
