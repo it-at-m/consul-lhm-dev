4c6e8facdd9f5b1f8657a69fce8c041198aca91f
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-09-25 10:13:22 +0300

    Update proposals in projekt footer

--- app/assets/javascripts/custom/radio_button_filter.js
+++ app/assets/javascripts/custom/radio_button_filter.js
@@ -1,16 +1,26 @@
 (function() {
+  "use strict";
+
   App.RadioButtonFilter = {
     initialize: function() {
       $(".js-radio-button-filter").on("change", function(e) {
-        var params = new URLSearchParams(window.location.search)
+        var params = new URLSearchParams(window.location.search);
 
-        var filterName = e.currentTarget.name
-        var filterValue = e.currentTarget.value
+        var radioButton = e.currentTarget;
+        var filterName = radioButton.name;
+        var filterValue = radioButton.value;
+        var anchor = radioButton.dataset.anchor;
 
-        params.set(filterName, filterValue)
+        params.set(filterName, filterValue);
 
-        Turbolinks.visit(window.location.pathname + '?' + params.toString())
+        var url = window.location.pathname + "?" + params.toString();
+
+        if (anchor) {
+          url = url + "#" + anchor;
+        }
+
+        Turbolinks.visit(url);
       });
     }
-  }
+  };
 }).call(this);
