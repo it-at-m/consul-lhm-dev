0801a94995ab1d12a4240864f4a8b223bfbf283c
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-10-13 10:06:54 +0300

    Update

--- app/assets/javascripts/custom/custom_tabs.js
+++ app/assets/javascripts/custom/custom_tabs.js
@@ -5,8 +5,10 @@
       console.log("Custom tabs init")
       $(".js-custom-tab").on("click", function(event) {
         event.preventDefault();
-        // var targetId = $(this).data('target');
-        // navigator.clipboard.writeText( $('#' + targetId).html().trim() );
+        var tabContentId = event.currentTarget.dataset.contentId;
+        var tabContent = document.getElementById(tabContentId);
+        $(".js-custom-tab-content").removeClass("-visible");
+        tabContent.classList.add("-visible");
       });
     }
   };
