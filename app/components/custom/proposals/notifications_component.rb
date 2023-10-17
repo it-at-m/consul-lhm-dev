d6064dff90b8d35826c3eaef006bc625f2793be9
Michael Svystun <michael.svystun@gmail.com>
2023-10-17 16:02:21 +0300

    con-1348

--- app/components/custom/proposals/notifications_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Proposals::NotificationsComponent < ApplicationComponent
-  def initialize(notifications:, author:)
-    @notifications = notifications
-    @author = author
-  end
-end
---
+++ app/components/custom/shared/notifications_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Shared::NotificationsComponent < ApplicationComponent
+  def initialize(notifiable:, author: nil)
+    @notifiable = notifiable
+    @author = author
+  end
+
+  def notifications
+    if @notifiable.is_a?(ProjektPhase::ProjektNotificationPhase)
+      @notifications ||= @notifiable.projekt_notifications
+
+    elsif @notifiable.is_a?(Proposal)
+      @notifications ||= @notifiable.proposal_notifications
+
+    else
+      @notifications ||= []
+    end
+  end
+end
