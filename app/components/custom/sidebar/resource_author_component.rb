428d3073ea68a5b5516db6dbfb7e6c5ea8f8b0c1
Michael Svystun <michael.svystun@gmail.com>
2023-10-18 11:09:53 +0300

    con-1346

--- app/components/custom/sidebar/resource_author_component.rb
+++ app/components/custom/sidebar/resource_author_component.rb
@@ -1,6 +1,8 @@
 class Sidebar::ResourceAuthorComponent < ApplicationComponent
   attr_reader :user
 
+  delegate :skip_user_verification?, to: :helpers
+
   def initialize(user:)
     @user = user
   end
