338d08bc9e476c5018f58c83f0e75bccebb9f2fd
Michael Svystun <michael.svystun@gmail.com>
2023-10-19 11:31:10 +0300

    con-1629

---
+++ app/components/custom/pages/projekts/sidebar_cta_component.rb
@@ -0,0 +1,36 @@
+class Pages::Projekts::SidebarCtaComponent < ApplicationComponent
+  def initialize(projekt_phase = nil)
+    @projekt_phase = projekt_phase
+  end
+
+  def render?
+    return false if @projekt_phase.nil?
+
+    @projekt_phase.type.in?(phase_types_with_new_button + phase_types_with_link)
+  end
+
+  private
+
+    def phase_types_with_new_button
+      %w[
+        ProjektPhase::DebatePhase
+        ProjektPhase::ProposalPhase
+        ProjektPhase::BudgetPhase
+      ]
+    end
+
+    def phase_types_with_link
+      %w[
+        ProjektPhase::VotingPhase
+        ProjektPhase::QuestionPhase
+      ]
+    end
+
+    def title_text
+      I18n.t("custom.projekt_phases.cta.title")
+    end
+
+    def button_text
+      I18n.t("custom.projekt_phases.cta.#{@projekt_phase.name}")
+    end
+end
