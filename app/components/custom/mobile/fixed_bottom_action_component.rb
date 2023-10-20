c35323900fe59489141e537e3a61c297a6634eb2
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-10-20 10:24:04 +0300

    Update mobile styles

---
+++ app/components/custom/mobile/fixed_bottom_action_component.rb
@@ -0,0 +1,2 @@
+class Mobile::FixedBottomActionComponent < ApplicationComponent
+end
--- app/components/custom/shared/sidebar_card_component.rb
+++ app/components/custom/shared/sidebar_card_component.rb
@@ -3,7 +3,10 @@
 class Shared::SidebarCardComponent < ApplicationComponent
   renders_many :additional_sections
 
-  def initialize(title: nil, description: nil, icon_name: "info", class_name: nil, collapsed_on_mobile: true)
+  def initialize(
+    title: nil, description: nil, icon_name: "info", class_name:
+    nil, collapsed_on_mobile: true
+  )
     @title = title
     @icon_name = icon_name
     @class_name = class_name
@@ -12,10 +15,12 @@ class Shared::SidebarCardComponent < ApplicationComponent
   end
 
   def class_name
+    base_class = @class_name || ""
+
     if @collapsed_on_mobile
-      "#{@class_name} -collapsed-on-mobile"
-    else
-      @class_name
+      base_class += " -collapsed-on-mobile"
     end
+
+    base_class
   end
 end
