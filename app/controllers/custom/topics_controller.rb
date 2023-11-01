3f01830d17c1b3a41382821d1fa6e1ffdd6b824b
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-11-01 14:46:40 +0200

    Update layout

--- app/controllers/custom/topics_controller.rb
+++ app/controllers/custom/topics_controller.rb
@@ -1,6 +1,16 @@
 require_dependency Rails.root.join("app", "controllers", "topics_controller").to_s
 
 class TopicsController < ApplicationController
+  def new
+    @topic = Topic.new
+
+    if Setting.new_design_enabled?
+      render :new_v2
+    else
+      render :new
+    end
+  end
+
   def show
     @commentable = @topic
     @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
