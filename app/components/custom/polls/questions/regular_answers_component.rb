83e5d07961589c1d617816691f825b4d5479f4c7
Michael Svystun <michael.svystun@gmail.com>
2023-11-13 14:58:54 +0200

    bes-750

--- app/components/custom/polls/questions/regular_answers_component.rb
+++ app/components/custom/polls/questions/regular_answers_component.rb
@@ -4,4 +4,12 @@ class Polls::Questions::RegularAnswersComponent < Polls::Questions::AnswersCompo
   def question_answers
     question.question_answers.where.not(open_answer: true)
   end
+
+  def answer_form_class(question_answer)
+    if question_answer.more_info_link.present? || show_additional_info_description?(question_answer)
+      "poll-answer-form--wide"
+    else
+      "poll-answer-form"
+    end
+  end
 end
