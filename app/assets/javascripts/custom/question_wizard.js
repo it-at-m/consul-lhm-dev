71ce2d252576831a5af45a2478cf61b3cbb18ab5
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-11-20 13:16:53 +0200

    Update polls

--- app/assets/javascripts/custom/question_wizard.js
+++ app/assets/javascripts/custom/question_wizard.js
@@ -4,8 +4,7 @@
     initialize: function() {
       $(".js-question-wizard").on("click", ".js-question-wizard-prev", this.goToPrevQuestion.bind(this));
       $(".js-question-wizard").on("click", ".js-question-wizard-next", this.goToNextQuestion.bind(this));
-
-      // $(".question-wizard-item form").on("ajax:success", this.navigateToQuestionById.bind(this));
+      $(".js-question-wizard").on("click", ".js-question-wizard-go-to-start", this.goToStart.bind(this));
     },
 
     currentQuestion: function() {
@@ -21,13 +20,27 @@
 
     goToNextQuestion: function() {
       var currentQuestion = this.currentQuestion();
-      var nextQuestion = currentQuestion.nextElementSibling;
+      var nextQuestionId = document.querySelector(".js-question-answered").dataset.nextQuestionId;
+      var nextQuestion;
+
+      if (nextQuestionId) {
+        nextQuestion = document.querySelector(
+          ".js-question-wizard [data-question-id='" + nextQuestionId + "']"
+        );
+      } else {
+        nextQuestion = currentQuestion.nextElementSibling;
+      }
 
       this.navigateToQuestion(nextQuestion);
     },
 
-    navigateToQuestionById: function(id) {
-      var nextQuestion = document.querySelector(".js-question-wizard [data-question-id='" + id + "']");
+    firstQuestion: function() {
+      return document.querySelector(".js-question-wizard .js-question-wizard-item:first-child");
+    },
+
+    goToStart: function() {
+      var nextQuestion = this.firstQuestion();
+
       this.navigateToQuestion(nextQuestion);
     },
 
@@ -40,17 +53,23 @@
       var $nextButton = $(".js-question-wizard-next");
 
       if (nextQuestion.nextElementSibling) {
-        $nextButton.css("visibility", "visible");
+        $nextButton.show();
       } else {
-        $nextButton.css("visibility", "hidden");
+        $nextButton.hide();
       }
 
       var $previousButton = $(".js-question-wizard-prev");
 
       if (nextQuestion.previousElementSibling) {
-        $previousButton.css("visibility", "visible");
+        $previousButton.show();
       } else {
-        $previousButton.css("visibility", "hidden");
+        $previousButton.hide();
+      }
+
+      if (this.firstQuestion() === nextQuestion) {
+        $(".js-question-wizard-go-to-start").hide();
+      } else {
+        $(".js-question-wizard-go-to-start").show();
       }
     }
   };
