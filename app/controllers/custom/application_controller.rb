8e47ebe74651f182e3f895d47db879294c11fbdf
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-12-01 12:12:52 +0200

    Update order of projekts

--- app/controllers/custom/application_controller.rb
+++ app/controllers/custom/application_controller.rb
@@ -59,6 +59,7 @@ class ApplicationController < ActionController::Base
     def set_projekts_for_overview_page_navigation
       @projekts_for_overview_page_navigation =
         Projekt
+          .sort_by_order_number
           .includes({page: [:translations]}, :projekt_settings, { children_projekts_show_in_navigation: :projekt_settings })
           .joins(:projekt_settings)
           .where(projekt_settings: { key: "projekt_feature.general.show_in_overview_page_navigation", value: "active" })
