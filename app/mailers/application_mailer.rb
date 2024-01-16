8625d7e89e95c2794a8e0af64b9ddc96b55fa182
Michael Svystun <michael.svystun@gmail.com>
2024-01-16 16:44:59 +0000

    minor fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,15 @@ class ApplicationMailer < ActionMailer::Base
   helper :application
   default from: proc { "#{Setting["mailer_from_name"]} <#{Setting["mailer_from_address"]}>" }
   layout "mailer"
+  before_action :set_bcc # custom
+
+  private
+
+    def set_bcc
+      bcc_email = Rails.application.secrets.bcc_email
+
+      if bcc_email.present?
+        headers["bcc"] = bcc_email
+      end
+    end
 end
--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,12 +2,21 @@ class DeviseMailer < Devise::Mailer
   helper :application, :settings, :mailer
   include Devise::Controllers::UrlHelpers
   default template_path: "devise/mailer"
+  before_action :set_bcc # custom
 
   protected
 
     def devise_mail(record, action, opts = {})
       I18n.with_locale record.locale do
         super(record, action, opts)
+      end
+    end
+
+    def set_bcc
+      bcc_email = Rails.application.secrets.bcc_email
+
+      if bcc_email.present?
+        headers["bcc"] = bcc_email
       end
     end
 end
