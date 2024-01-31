bd09311f303bb77c42f0718df6308a1a62a428ca
Michael Svystun <michael.svystun@gmail.com>
2024-01-31 11:27:34 +0000

    con-1814

--- app/components/custom/admin/menu_component.rb
+++ app/components/custom/admin/menu_component.rb
@@ -6,7 +6,7 @@ class Admin::MenuComponent < ApplicationComponent
   private
 
     def booths?
-      %w[officers booths shifts booth_assignments officer_assignments].include?(controller_name) && controller.class.parent == Admin::Poll ||
+      %w[officers booths shifts booth_assignments officer_assignments].include?(controller_name) && controller.class.module_parent == Admin::Poll ||
         controller_name == "polls" && action_name == "booth_assignments"
     end
 
@@ -20,7 +20,7 @@ class Admin::MenuComponent < ApplicationComponent
 
     def deficiency_reports?
       %w[officers categories statuses settings areas].include?(controller_name) &&
-        controller.class.parent == Admin::DeficiencyReports
+        controller.class.module_parent == Admin::DeficiencyReports
     end
 
     def deficiency_reports_list
@@ -35,7 +35,7 @@ class Admin::MenuComponent < ApplicationComponent
       [
         t("custom.admin.menu.deficiency_reports.officers"),
         admin_deficiency_report_officers_path,
-        controller_name == "officers" && controller.class.parent == Admin::DeficiencyReports
+        controller_name == "officers" && controller.class.module_parent == Admin::DeficiencyReports
       ]
     end
 
@@ -43,7 +43,7 @@ class Admin::MenuComponent < ApplicationComponent
       [
         t("custom.admin.menu.deficiency_reports.categories"),
         admin_deficiency_report_categories_path,
-        controller_name == "categories" && controller.class.parent == Admin::DeficiencyReports
+        controller_name == "categories" && controller.class.module_parent == Admin::DeficiencyReports
       ]
     end
 
@@ -51,7 +51,7 @@ class Admin::MenuComponent < ApplicationComponent
       [
         t("custom.admin.menu.deficiency_reports.statuses"),
         admin_deficiency_report_statuses_path,
-        controller_name == "statuses" && controller.class.parent == Admin::DeficiencyReports
+        controller_name == "statuses" && controller.class.module_parent == Admin::DeficiencyReports
       ]
     end
 
@@ -59,7 +59,7 @@ class Admin::MenuComponent < ApplicationComponent
       [
         t("custom.admin.menu.deficiency_reports.settings"),
         admin_deficiency_report_settings_path,
-        controller_name == "settings" && controller.class.parent == Admin::DeficiencyReports
+        controller_name == "settings" && controller.class.module_parent == Admin::DeficiencyReports
       ]
     end
 
@@ -67,7 +67,7 @@ class Admin::MenuComponent < ApplicationComponent
       [
         t("custom.admin.menu.deficiency_reports.areas"),
         admin_deficiency_report_areas_path,
-        controller_name == "areas" && controller.class.parent == Admin::DeficiencyReports
+        controller_name == "areas" && controller.class.module_parent == Admin::DeficiencyReports
       ]
     end
 
@@ -88,7 +88,7 @@ class Admin::MenuComponent < ApplicationComponent
       controllers_names = ["settings", "tags", "geozones", "images", "content_blocks",
                            "local_census_records", "imports", "age_restrictions", "individual_groups", "individual_group_values"]
       controllers_names.include?(controller_name) &&
-        controller.class.parent != Admin::Poll::Questions::Answers &&
+        controller.class.module_parent != Admin::Poll::Questions::Answers &&
         controller.class != Admin::DeficiencyReports::SettingsController
     end
 
