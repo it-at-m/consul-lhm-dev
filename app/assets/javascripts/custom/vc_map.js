d92fe6d2e993fc933db9c82445ec8ef18c125c31
Michael Svystun <michael.svystun@gmail.com>
2024-01-31 12:04:47 +0000

    con-1832

--- app/assets/javascripts/custom/vc_map.js
+++ app/assets/javascripts/custom/vc_map.js
@@ -260,7 +260,7 @@
           $(app.customMapOptions.latitudeInputSelector).val(wgs84coordinates[1]);
           $(app.customMapOptions.longitudeInputSelector).val(wgs84coordinates[0]);
           $(app.customMapOptions.altitudeInputSelector).val(wgs84coordinates[2]);
-          $(app.customMapOptions.zoomInputSelector).val(10); // TODO: fix this line
+          $(app.customMapOptions.zoomInputSelector).val(app.customMapOptions.mapCenterZoom);
           $(app.customMapOptions.shapeInputSelector).val(JSON.stringify({}));
 
         } else if (geometry instanceof ol.geom.Polygon) {
@@ -282,7 +282,7 @@
 
           $(app.customMapOptions.latitudeInputSelector).val(wgs84coordinates[0][1]);
           $(app.customMapOptions.longitudeInputSelector).val(wgs84coordinates[0][0]);
-          $(app.customMapOptions.zoomInputSelector).val(10); // TODO: fix this line
+          $(app.customMapOptions.zoomInputSelector).val(app.customMapOptions.mapCenterZoom);
           $(app.customMapOptions.shapeInputSelector).val(shapeString);
         }
 
