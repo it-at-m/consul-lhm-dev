afdc469a1e2e76fe5f50cf407f57abfac09622e7
Michael Svystun <michael.svystun@gmail.com>
2024-02-07 13:44:56 +0000

    sync

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,19 @@ set :keep_releases, 5
 
 set :local_user, ENV["USER"]
 
+set :fnm_path, "$HOME/.fnm"
+set :fnm_install_command, "curl -fsSL https://fnm.vercel.app/install | " \
+                          "bash -s -- --install-dir \"#{fetch(:fnm_path)}\""
+set :fnm_update_command, "#{fetch(:fnm_install_command)} --skip-shell"
+set :fnm_setup_command, -> do
+                          "export PATH=\"#{fetch(:fnm_path)}:$PATH\" && " \
+                            "cd #{release_path} && fnm env > /dev/null && eval \"$(fnm env)\""
+                        end
+set :fnm_install_node_command, -> { "#{fetch(:fnm_setup_command)} && fnm use --install-if-missing" }
+set :fnm_map_bins, %w[bundle node npm puma pumactl rake yarn]
+
 set :puma_conf, "#{release_path}/config/puma/#{fetch(:rails_env)}.rb"
+set :puma_systemctl_user, :user
 
 set :delayed_job_workers, 2
 set :delayed_job_roles, :background
@@ -36,20 +48,16 @@ set :delayed_job_roles, :background
 set :whenever_roles, -> { :app }
 
 namespace :deploy do
-  Rake::Task["delayed_job:default"].clear_actions
-  Rake::Task["puma:smart_restart"].clear_actions
+  after "rvm1:hook", "map_node_bins"
 
+  after :updating, "install_node"
   after :updating, "install_ruby"
 
   after "deploy:migrate", "add_new_settings"
 
   after :publishing, "setup_puma"
 
-  after :published, "deploy:restart"
-  before "deploy:restart", "puma:restart"
-  before "deploy:restart", "restart_delayed_jobs"
-  before "deploy:restart", "puma:start"
-
+  before "puma:smart_restart", "stop_puma_daemon"
   after :finished, "refresh_sitemap"
 
   desc "Deploys and runs the tasks needed to upgrade to a new release"
@@ -57,6 +65,9 @@ namespace :deploy do
     after "add_new_settings", "execute_release_tasks"
     invoke "deploy"
   end
+
+  before "deploy:restart", "puma:smart_restart"
+  before "deploy:restart", "restart_delayed_jobs"
 end
 
 task :install_ruby do
@@ -73,6 +84,40 @@ task :install_ruby do
           after "install_ruby", "rvm1:install:ruby"
         else
           info "Ruby: Using #{current_ruby}"
+        end
+      end
+    end
+  end
+end
+
+task :install_node do
+  on roles(:app) do
+    with rails_env: fetch(:rails_env) do
+      begin
+        execute fetch(:fnm_install_node_command)
+      rescue SSHKit::Command::Failed
+        begin
+          execute fetch(:fnm_setup_command)
+        rescue SSHKit::Command::Failed
+          execute fetch(:fnm_install_command)
+        else
+          execute fetch(:fnm_update_command)
+        end
+
+        execute fetch(:fnm_install_node_command)
+      end
+    end
+  end
+end
+
+task :map_node_bins do
+  on roles(:app) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        prefix = -> { "#{fetch(:fnm_path)}/fnm exec" }
+
+        fetch(:fnm_map_bins).each do |command|
+          SSHKit.config.command_map.prefix[command.to_sym].unshift(prefix)
         end
       end
     end
@@ -127,10 +172,30 @@ end
 
 desc "Create pid and socket folders needed by puma"
 task :setup_puma do
-  on roles(:app) do
+  on roles(fetch(:puma_role)) do
     with rails_env: fetch(:rails_env) do
       execute "mkdir -p #{shared_path}/tmp/sockets; true"
       execute "mkdir -p #{shared_path}/tmp/pids; true"
+    end
+  end
+
+  after "setup_puma", "puma:systemd:config"
+  after "setup_puma", "puma:systemd:enable"
+end
+
+# Code adapted from the task to stop the daemon in capistrano3-puma
+desc "Stops the Puma daemon so systemd can start the Puma process"
+task :stop_puma_daemon do
+  on roles(fetch(:puma_role)) do |role|
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        if test("[ -f #{fetch(:puma_pid)} ]") &&
+           !test("systemctl --user is-active #{fetch(:puma_service_unit_name)}") &&
+           test(:kill, "-0 $( cat #{fetch(:puma_pid)} )")
+          info "Puma: stopping daemon"
+          execute :pumactl, "-S #{fetch(:puma_state)} -F #{fetch(:puma_conf)} stop"
+        end
+      end
     end
   end
 end
